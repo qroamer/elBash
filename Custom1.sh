896d1e4a3c2915cdc0f4df285c9a513cd7ab5d62
qroamer <qroamer@gmail.com>
2018-11-13 12:05:02 -0500

    Refined Sudo Check and updated README; also changed mode to executable.

--- Custom1.sh
+++ Custom1.sh
@@ -3,19 +3,18 @@
 #   Atom, Steam, htop, elementary tweaks, chromium
 
 # Check to see if script is running as root.
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
+
+if [[ $(id -u) -ne 0 ]] ;
+  then echo "Please run as root"
+  exit 1
+else
+  # INSTALL THAT SHiT
+  apt-get install software-properties-common
+  # This installs the necessary
+  add-apt-repository -y ppa:philip.scott/elementary-tweaks
+  add-apt-repository -y ppa:webupd8team/atom
+  add-apt-repository -y ppa:dawidd0811/neofetch
+  apt-get update
+  apt-get install -y elementary-tweaks htop chromium-browser atom neofetch
 fi
-
-# INSTALL THAT SHIT
-apt-get install software-properties-common
-# This installs the necessary
-add-apt-repository -y ppa:philip.scott/elementary-tweaks
-add-apt-repository -y ppa:webupd8team/atom
-add-apt-repository -y ppa:dawidd0811/neofetch
-apt-get update
-
-apt-get install -y elementary-tweaks htop chromium-browser atom neofetch
-
 neofetch
