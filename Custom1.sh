36fe2ebd0a79d65993b3b77a64d896bda2b68ad1
qroamer <qroamer@gmail.com>
2018-11-13 11:33:01 -0500

    Edits to install ElementaryTweaks,Htop,ChromiumBrowser,atom & neofetch

--- Custom1.sh
+++ Custom1.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
+# Programs to install:
+#   Atom, Steam, htop, elementary tweaks, chromium
+#
+
+apt-get install software-properties-common
+add-apt-repository -y ppa:philip.scott/elementary-tweaks
+add-apt-repository -y ppa:webupd8team/atom
+add-apt-repository -y ppa:dawidd0811/neofetch
 apt-get update
+
+apt-get install -y elementary-tweaks htop chromium-browser atom neofetch
+
+neofetch
