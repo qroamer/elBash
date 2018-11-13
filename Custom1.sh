1a44fd5744805e16461398d1da3adba54d4f764c
qroamer <qroamer@gmail.com>
2018-11-13 11:46:30 -0500

    Added sudo check and added readme

--- Custom1.sh
+++ Custom1.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 # Programs to install:
 #   Atom, Steam, htop, elementary tweaks, chromium
-#
 
+# Check to see if script is running as root.
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# INSTALL THAT SHIT
 apt-get install software-properties-common
+# This installs the necessary
 add-apt-repository -y ppa:philip.scott/elementary-tweaks
 add-apt-repository -y ppa:webupd8team/atom
 add-apt-repository -y ppa:dawidd0811/neofetch
---
+++ README.txt
@@ -0,0 +1,3 @@
+You'll need to run "chmod +x Custom1.sh" to be able to run this script.
+
+Script needs to be run as root, and the command will be "sudo sh Custom1.sh"
