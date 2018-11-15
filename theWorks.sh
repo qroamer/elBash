99d41721807f06d42adb190ef97a6e644e04f6dc
qroamer <qroamer@gmail.com>
2018-11-15 14:30:28 -0500

    Adjusted APM installations.

--- theWorks.sh
+++ theWorks.sh
@@ -8,18 +8,26 @@ if [[ $(id -u) -ne 0 ]] ;
   then echo "Please run as root"
   exit 1
 else
-  # INSTALL THAT SHiT
-  apt-get install -y software-properties-common
-  # This installs the necessary
-  sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
-  sudo add-apt-repository -y ppa:webupd8team/atom
-  sudo add-apt-repository -y ppa:dawidd0811/neofetch
-  sudo apt-get update
-  sudo apt-get install -y elementary-tweaks htop chromium-browser atom neofetch npm steam
-  sudo npm install -g node-gyp
-  apm install git-plus
-  apm install github-plus
-  apm install termination
-  apm install sublime-style-column-selection
+
+# INSTALL THAT SHiT
+apt-get install -y software-properties-common
+
+# This installs the necessary repositories
+sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo add-apt-repository -y ppa:dawidd0811/neofetch
+
+sudo apt-get update
+
+sudo apt-get install -y elementary-tweaks htop chromium-browser atom neofetch npm steam
+
+sudo npm install -g node-gyp
+
+apm install git-plus
+apm install github-plus
+apm install terminal-plus
+apm delete terminal-plus
+apm install termination
+apm install sublime-style-column-selection
 fi
 neofetch
